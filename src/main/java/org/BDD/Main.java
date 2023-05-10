17c694fc5defa10cd86c8e837de90c3239729074
Emanuele <leledubini0@gmail.com>
2023-05-10 17:53:46 +0200

    chlesky linear sistem resolution and added code for result csv file generation

--- src/main/java/org/BDD/Main.java
+++ src/main/java/org/BDD/Main.java
@@ -1,11 +1,12 @@
 package org.BDD;
 
+import com.opencsv.CSVWriter;
+import org.ejml.EjmlParameters;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.data.DMatrixSparseCSC;
 import org.ejml.interfaces.linsol.LinearSolverSparse;
 import org.ejml.sparse.FillReducing;
 import org.ejml.sparse.csc.CommonOps_MT_DSCC;
-import org.ejml.sparse.csc.MatrixFeatures_DSCC;
 import org.ejml.sparse.csc.NormOps_DSCC;
 import org.ejml.sparse.csc.factory.LinearSolverFactory_DSCC;
 import us.hebi.matlab.mat.ejml.Mat5Ejml;
@@ -16,23 +17,35 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.instrument.Instrumentation;
+import java.lang.management.MemoryUsage;
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        ArrayList<String> MatrixName = new ArrayList<>();
+        ArrayList<Long> Size = new ArrayList<>();
+        ArrayList<Long> MemoryPre = new ArrayList<>();
+        ArrayList<Long> MemoryPost = new ArrayList<>();
+        ArrayList<Long> MemoryDiff = new ArrayList<>();
+        ArrayList<Double> Time = new ArrayList<>();
+        ArrayList<Double> Error = new ArrayList<>(); //todo mettere in notazione scientifica
+
         // Percorso della cartella contenente le matrici
         String path = "src/main/java/org/BDD/Matrici/";
+        File matriciFolder = new File(path);
         // Lista di tutti i file nella cartella Matrici
-        File[] files = new File(path).listFiles();
+        File[] files = matriciFolder.listFiles();
 
         //----------CICLO CHE LEGGE TUTTE LE MATRICI DELLA CARTELLA "Matrici"----------
-        for (File file : files) {
-            if (file.isFile()) {
-                System.out.println("-----------------------------------" + "Elaborazione della matrice " + file.getName() + "-----------------------------------");
+        for (int i=0; i<files.length; i++) {
+            if (files[i].isFile()) {
+                System.out.println("-----------------------------------" + "Elaborazione della matrice " + files[i].getName() + "-----------------------------------");
 
                 // Importazione della matrice sparsa simmetrica e definita positiva A
-                Sparse value = Mat5.readFromFile(file.getAbsolutePath())
+                Sparse value = Mat5.readFromFile(files[i].getAbsolutePath())
                         .getStruct("Problem")
                         .getSparse("A");
 
@@ -44,7 +57,14 @@ public class Main {
                 System.out.println("Numero di elementi: " + A.getNumElements());
                 System.out.println("Numero di elementi non nulli: " + A.nz_length);
 
-                System.out.println("\n---> Inizio elaborazione matrice " + file.getName() + " \n");
+                File matriceA = new File("src/main/java/org/BDD/Matrici/" + files[i].getName());
+                long dimensionA = matriceA.length();
+                Size.add(dimensionA);
+                System.out.println("Dimensioni matrice A: " + dimensionA / (1024 * 1024) + " MB.");
+
+                System.out.println("\n---> Inizio elaborazione matrice " + files[i].getName() + " \n");
+                MatrixName.add(files[i].getName());
+
 
                 //----------CONTROLLO CHE LA MATRICE SIA DEFINITA POSITIVA E SIMMETRICA---------- todo rimettere per consegna
                 /*if(MatrixFeatures_DSCC.isPositiveDefinite(A)){
@@ -68,6 +88,7 @@ public class Main {
                 System.gc();
                 // Misura la memoria iniziale
                 long memoriaIniziale = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+                MemoryPre.add(memoriaIniziale);
 
                 //calcola la dimensione della matrice A
                 int n = A.numCols; //la matrice è simmetrica quindi n = m
@@ -75,8 +96,8 @@ public class Main {
                 //Crea il vettore B di modo che x = [1,1,....,1]
                 //tmp è un vettore colonna, va creato il vettore di tutti 1 e poi moltiplicato per la matrice A per creare B
                 DMatrixSparseCSC tmp  = new DMatrixSparseCSC (n,1);
-                for(int i = 0; i < n; i++){
-                    tmp.set(i,0,1);
+                for(int j = 0; j < n; j++){
+                    tmp.set(j,0,1);
                 }
 
                 //moltiplicazione tra il vettore di tutti 1 tmp e la matrice A, il risultato viene salvato in B
@@ -104,9 +125,11 @@ public class Main {
 
                 // Misura la memoria finale
                 long memoriaFinale = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+                MemoryPost.add(memoriaFinale);
 
                 // Calcola la memoria utilizzata
                 long memoriaUtilizzata = memoriaFinale - memoriaIniziale;
+                MemoryDiff.add(memoriaUtilizzata);
                 System.out.println("Memoria iniziale: " + (memoriaIniziale / (1024F * 1024F)) + " MB");
                 System.out.println("Memoria finale: " + (memoriaFinale / (1024F * 1024F)) + " MB");
                 System.out.println("Memoria utilizzata nella risoluzione: " + (memoriaUtilizzata / (1024F * 1024F)) + " MB");
@@ -115,13 +138,14 @@ public class Main {
                 long stopTime = System.currentTimeMillis();
                 //calcola il tempo impiegato in millisecondi
                 double elapsedTimeSeconds = (stopTime - startTime) / 1000.0;
+                Time.add(elapsedTimeSeconds);
                 System.out.println("Tempo di esecuzione: " + elapsedTimeSeconds + " s");
 
                 //----------CALCOLO ERRORE RELATIVO----------
                 //definisco vettore soluzione xe esatta di modo che xe = [1,1,....,1]
                 DMatrixSparseCSC xe  = new DMatrixSparseCSC (n,1);
-                for(int i = 0; i < n; i++){
-                    xe.set(i,0,1);
+                for(int z = 0; z < n; z++){
+                    xe.set(z,0,1);
                 }
 
                 double norm_x = NormOps_DSCC.normF(x);
@@ -130,10 +154,17 @@ public class Main {
                 double relative_error = norm_diff / norm_xe;
                 //System.out.println("Norma di x: " + norm_x);
                 //System.out.println("Norma di xe: " + norm_xe);
+                Error.add(relative_error);
                 System.out.println("Errore relativo: " + relative_error);
                 System.out.println("\n");
             }
         }
+        //----------FINE CICLO FOR----------
+        System.out.println("\n----------------------------------------------------------------------");
+
+        //----------SCRITTURA FILE CSV----------
+        write("src/main/java/org/BDD/Dati_Java.csv", MatrixName, Size, MemoryPre, MemoryPost, MemoryDiff, Time, Error);
+        System.out.println("\nFile CSV creato");
 
 
         // importazione della matrice sparsa simmetrica e definita positiva A
@@ -146,6 +177,25 @@ public class Main {
         A.nz_length = value.getNumNonZero();*/
        }
 
+    public static void write(String filePath, ArrayList<String> MatrixName, ArrayList<Long> Size, ArrayList<Long> MemoryPre,
+                             ArrayList<Long> MemoryPost, ArrayList<Long> MemoryDiff, ArrayList<Double> Time,
+                             ArrayList<Double> Error) throws IOException {
+        FileWriter outputFile = new FileWriter(filePath);
+        CSVWriter writer = new CSVWriter(outputFile);
+
+        String[] header = {"MatrixName", "Size", "MemoryPre", "MemoryPost", "MemoryDiff", "Time", "Error"};
+        writer.writeNext(header, false);
+
+        int n = MatrixName.size();
+        for (int i = 0; i < n; i++) {
+            String[] row = {MatrixName.get(i), Size.get(i).toString(), MemoryPre.get(i).toString(),
+                    MemoryPost.get(i).toString(), MemoryDiff.get(i).toString(),
+                    Time.get(i).toString(), Error.get(i).toString()};
+            writer.writeNext(row, false);
+        }
+
+        writer.close();
+    }
        public static void matrixTXT(DMatrixSparseCSC A, String matrixName) {
            /*try {
                // Open a file for writing
